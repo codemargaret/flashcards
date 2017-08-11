e2cd11e709fc90a93f09386259f1c9439eb80c99
@mdawg85 <margaretshelaghmcgovern@gmail.com>
2017-08-10 21:34:39 -0700

    add classes to wells, streamline js and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,83 +1,8 @@
 $(document).ready(function() {
-  $(".clickable1").click(function() {
-    $(".display1").slideToggle(600);
-    $(".hide1").slideToggle(600);
-  });
-  $(".clickable2").click(function() {
-    $(".display2").slideToggle(600);
-    $(".hide2").slideToggle(600);
-  });
-  $(".clickable3").click(function() {
-    $(".display3").slideToggle(600);
-    $(".hide3").slideToggle(600);
-  });
-  $(".clickable4").click(function() {
-    $(".display4").slideToggle(600);
-    $(".hide4").slideToggle(600);
-  });
-  $(".clickable5").click(function() {
-    $(".display5").slideToggle(600);
-    $(".hide5").slideToggle(600);
-  });
-  $(".clickable6").click(function() {
-    $(".display6").slideToggle(600);
-    $(".hide6").slideToggle(600);
-  });
-  $(".clickable7").click(function() {
-    $(".display7").slideToggle(600);
-    $(".hide7").slideToggle(600);
-  });
-  $(".clickable8").click(function() {
-    $(".display8").slideToggle(600);
-    $(".hide8").slideToggle(600);
-  });
-  $(".clickable9").click(function() {
-    $(".display9").slideToggle(600);
-    $(".hide9").slideToggle(600);
-  });
-  $(".clickable10").click(function() {
-    $(".display10").slideToggle(600);
-    $(".hide10").slideToggle(600);
-  });
-  $(".clickable11").click(function() {
-    $(".display11").slideToggle(600);
-    $(".hide11").slideToggle(600);
-  });
-  $(".clickable12").click(function() {
-    $(".display12").slideToggle(600);
-    $(".hide12").slideToggle(600);
-  });
-  $(".clickable13").click(function() {
-    $(".display13").slideToggle(600);
-    $(".hide13").slideToggle(600);
-  });
-  $(".clickable14").click(function() {
-    $(".display14").slideToggle(600);
-    $(".hide14").slideToggle(600);
-  });
-  $(".clickable15").click(function() {
-    $(".display15").slideToggle(600);
-    $(".hide15").slideToggle(600);
-  });
-  $(".clickable16").click(function() {
-    $(".display16").slideToggle(600);
-    $(".hide16").slideToggle(600);
-  });
-  $(".clickable17").click(function() {
-    $(".display17").slideToggle(600);
-    $(".hide17").slideToggle(600);
-  });
-  $(".clickable18").click(function() {
-    $(".display18").slideToggle(600);
-    $(".hide18").slideToggle(600);
-  });
-  $(".clickable19").click(function() {
-    $(".display19").slideToggle(600);
-    $(".hide19").slideToggle(600);
-  });
-  $(".clickable20").click(function() {
-    $(".display20").slideToggle(600);
-    $(".hide20").slideToggle(600);
+  $(".clickable").click(function(event) {
+    console.log($(this).parent().html());
+    $(".front").slideToggle(600);
+    $(".back").slideToggle(600);
   });
   $("button").click(function(){
     $("ul").append("<li>You're doing great!</li>");
