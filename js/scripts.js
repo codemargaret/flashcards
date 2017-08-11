3d8082890aac1ad519a269fe57b4d1aa3b7f816e
@mdawg85 <margaretshelaghmcgovern@gmail.com>
2017-08-10 22:19:25 -0700

    add final touches

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $(this).parents('.well').children('.front').slideToggle(600);
-    $(this).parents('.well').children('.back').slideToggle(600);
+    $(this).parents('.well').children('.front, .back').slideToggle(600);
     // $(".front").slideToggle(600);
     // $(".back").slideToggle(600);
   });
